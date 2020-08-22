fd8b3d1dda778e76ea54e96079e670b470e9621b
unknown <valericfbg@gmail.com>
2020-08-22 11:30:04 +0300

    receiving and parsing connection bytes to Request obj

--- connection/connectionhandler.go
+++ connection/connectionhandler.go
@@ -2,14 +2,25 @@ package connection
 
 import (
     "net"
-    "io"
     "log"
+    "bufio"
+    "net/http"
 )
 
-func HandleConnection(c net.Conn) {
-    io.Copy(c, c)
+func HandleConnection(c net.Conn) error {
     log.Println("received connection")
+    reader := bufio.NewReader(c)
+    req, err := http.ReadRequest(reader)
+    
+    if err != nil {
+        return err
+    }
+
+    log.Printf("Incoming request on path: %s and method: %s", req.URL, req.Method)
+    
     c.Close()
+
+    return nil
 }
 
 
